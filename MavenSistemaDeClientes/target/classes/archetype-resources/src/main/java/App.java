1bba7c2500b59bc672771ad80dc68599980363b8
Giovanni Edgardo Aedo Benavides <gaedob@gmail.com>
2024-08-19 12:12:11 -0400

    InicialVersion

---
+++ MavenSistemaDeClientes/target/classes/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $dev.tiktaalik;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
